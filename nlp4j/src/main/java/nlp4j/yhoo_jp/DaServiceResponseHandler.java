648b8d34e5b2adb758297a55c0bf03eaf62be63f
oyahiroki <oyahiroki@192.168.2.63>
2019-07-03 14:25:51 +0900

    20190703a

---
+++ nlp4j/src/main/java/nlp4j/yhoo_jp/DaServiceResponseHandler.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package nlp4j.yhoo_jp;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * @author oyahiroki
+ *
+ */
+public class DaServiceResponseHandler extends DefaultHandler {
+
+}
---
+++ nlp4j/src/main/java/nlp4j/yhoo_jp/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Yahoo Japan Developer Network - Text Analytics
+ * https://developer.yahoo.co.jp/webapi/jlp/
+ */
+package nlp4j.yhoo_jp;
