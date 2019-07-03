d6d1f780eff639797ffbd26d2e5d2c4df9ba3064
oyahiroki <oyahiroki@192.168.2.63>
2019-07-03 09:24:52 +0900

    Initial

---
+++ nlp4j/src/main/java/nlp4j/Hello.java
@@ -0,0 +1,10 @@
+package nlp4j;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.err.println("Hello");
+
+	}
+
+}
---
+++ nlp4j/src/test/java/nlp4j/HelloTest.java
@@ -0,0 +1,9 @@
+package nlp4j;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		System.err.println("Test");
+	}
+
+}
