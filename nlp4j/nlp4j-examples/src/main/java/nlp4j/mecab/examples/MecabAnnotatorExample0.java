07d4c04a59830986819e86bfefd432032fb15922
oyahiroki <12620956+oyahiroki@users.noreply.github.com>
2021-07-18 15:57:24 +0900

    work20210718b

---
+++ nlp4j/nlp4j-examples/src/main/java/nlp4j/mecab/examples/MecabAnnotatorExample0.java
@@ -0,0 +1,35 @@
+package nlp4j.mecab.examples;
+
+import nlp4j.Document;
+import nlp4j.Keyword;
+import nlp4j.impl.DefaultDocument;
+import nlp4j.mecab.MecabAnnotator;
+
+/**
+ * @author Hiroki Oya
+ * @created_at 2021-07-18
+ */
+public class MecabAnnotatorExample0 {
+
+	@SuppressWarnings("javadoc")
+	public static void main(String[] args) throws Exception {
+		// 自然文のテキスト
+		String text = "私は学校に行きました。";
+		Document doc = new DefaultDocument();
+		{
+			doc.putAttribute("text", text);
+		}
+
+		MecabAnnotator annotator = new MecabAnnotator();
+		{
+			annotator.setProperty("target", "text");
+		}
+		annotator.annotate(doc); // throws Exception
+
+		for (Keyword kwd : doc.getKeywords()) {
+			System.err.println(kwd);
+		}
+
+	}
+
+}
