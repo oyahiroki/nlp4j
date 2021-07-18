2f85d24054b0b11bc86790fc8cbed66ed9022da3
oyahiroki <12620956+oyahiroki@users.noreply.github.com>
2021-07-18 19:13:20 +0900

    work20210718b

---
+++ nlp4j/nlp4j-examples/src/main/java/nlp4j/cabocha/examples/CabochaAnnotatorExample0.java
@@ -0,0 +1,37 @@
+package nlp4j.cabocha.examples;
+
+import nlp4j.Document;
+import nlp4j.Keyword;
+import nlp4j.KeywordWithDependency;
+import nlp4j.cabocha.CabochaAnnotator;
+import nlp4j.impl.DefaultDocument;
+
+/**
+ * @author Hiroki Oya
+ * @created_at 2021-07-18
+ */
+public class CabochaAnnotatorExample0 {
+
+	@SuppressWarnings("javadoc")
+	public static void main(String[] args) throws Exception {
+		Document doc = new DefaultDocument();
+		{
+			doc.putAttribute("text", "私は大学に歩いて行きました。");
+		}
+		CabochaAnnotator ann = new CabochaAnnotator();
+		{
+			ann.setProperty("tempDir", "R:/");
+			ann.setProperty("encoding", "MS932");
+			ann.setProperty("target", "text");
+		}
+
+		ann.annotate(doc); // Annotation
+
+		for (Keyword kwd : doc.getKeywords()) {
+			if (kwd instanceof KeywordWithDependency) {
+				System.err.println(((KeywordWithDependency) kwd).toStringAsXml());
+			}
+		}
+	}
+
+}
