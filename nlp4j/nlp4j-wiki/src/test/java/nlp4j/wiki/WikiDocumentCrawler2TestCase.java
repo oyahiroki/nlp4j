58c0a63ccb24a18423ae8e0f1488a8cc8ee468a8
oyahiroki <12620956+oyahiroki@users.noreply.github.com>
2021-09-21 23:14:22 +0900

    work20210921b

---
+++ nlp4j/nlp4j-wiki/src/test/java/nlp4j/wiki/WikiDocumentCrawler2TestCase.java
@@ -0,0 +1,42 @@
+package nlp4j.wiki;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+import nlp4j.Document;
+
+public class WikiDocumentCrawler2TestCase extends TestCase {
+
+	Class target = WikiDocumentCrawler2.class;
+
+	public void test001() throws Exception {
+
+//		String wikidumpfile_name = "C:/usr/local/data/wiki/"
+//				+ "jawiktionary-20210401-pages-articles-multistream.xml.bz2";
+//		String wikiindexfile_name = "C:/usr/local/data/wiki/"
+//				+ "jawiktionary-20210401-pages-articles-multistream-index.txt.bz2";
+//		String entries = "学校,病院,医者,いぬ,ねこ";
+//
+//		WikiDocumentCrawler2 crawler = new WikiDocumentCrawler2();
+//		{
+//			crawler.setProperty("wikidumpfile", wikidumpfile_name);
+//			crawler.setProperty("wikiindexfile", wikiindexfile_name);
+//			crawler.setProperty("entries", entries);
+//		}
+//
+//		List<Document> docs = crawler.crawlDocuments();
+//
+//		for (Document doc : docs) {
+////			System.err.println(doc);
+//			System.err.println("---");
+//			System.err.println(doc.getAttribute("item"));
+//			System.err.println("---");
+//			System.err.println(doc.getAttribute("wikitext"));
+//			System.err.println("---");
+//			System.err.println(doc.getAttribute("wikiplaintext"));
+//			System.err.println("---");
+//			System.err.println(doc.getAttribute("wikihtml"));
+//		}
+	}
+
+}
