05783bd3da1d5379de71411d27f2e9313951280c
oyahiroki <12620956+oyahiroki@users.noreply.github.com>
2021-10-09 12:13:26 +0900

    work20211009a

---
+++ nlp4j/nlp4j-examples/src/main/java/nlp4j1crawler/CsvFileCrawlerExample.java
@@ -0,0 +1,23 @@
+package nlp4j1crawler;
+
+import java.util.List;
+
+import nlp4j.Document;
+import nlp4j.crawler.CsvFileCrawler;
+import nlp4j.util.DocumentUtil;
+
+public class CsvFileCrawlerExample {
+
+	public static void main(String[] args) throws Exception {
+
+		CsvFileCrawler crawler = new CsvFileCrawler();
+
+		crawler.setProperty("file", "src/main/resources/nlp4j1crawler/example_utf8.csv");
+
+		List<Document> docs = crawler.crawlDocuments();
+
+		System.err.println(DocumentUtil.toJsonPrettyString(docs));
+
+	}
+
+}
---
+++ nlp4j/nlp4j-examples/src/main/java/nlp4j1crawler/JsonFileCrawlerExample.java
@@ -0,0 +1,19 @@
+package nlp4j1crawler;
+
+import java.util.List;
+
+import nlp4j.Document;
+import nlp4j.crawler.JsonLineSeparatedCrawler;
+
+public class JsonFileCrawlerExample {
+
+	public static void main(String[] args) throws Exception {
+		JsonLineSeparatedCrawler crawler = new JsonLineSeparatedCrawler();
+		crawler.setProperty("file", "src/main/resources/nlp4j1crawler/example_json.txt");
+		List<Document> docs = crawler.crawlDocuments();
+		for (Document doc : docs) {
+			System.err.println(doc.getAttribute("text"));
+		}
+	}
+
+}
