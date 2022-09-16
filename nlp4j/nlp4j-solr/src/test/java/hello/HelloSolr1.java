6ea2f524748402dd8230a8876bcf101520c5b2dc
Hiroki Oya <12620956+oyahiroki@users.noreply.github.com>
2022-09-17 01:12:32 +0900

    work20220917a - Updated Solr Search Example

---
+++ nlp4j/nlp4j-solr/src/test/java/hello/HelloSolr1.java
@@ -0,0 +1,36 @@
+package hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrRequest.METHOD;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.params.MultiMapSolrParams;
+
+public class HelloSolr1 {
+
+	public static void main(String[] args) throws Exception {
+
+		String endPoint = "http://localhost:8983/solr/";
+		String collection = "sandbox";
+
+		HttpSolrClient solrClient = new HttpSolrClient.Builder(endPoint) //
+				.withConnectionTimeout(10 * 1000)//
+				.withSocketTimeout(60 * 1000)//
+				.build();
+
+		final Map<String, String[]> requestParamsSolr = new HashMap<>();
+
+		requestParamsSolr.put("q", new String[] { "*:*" });
+
+		MultiMapSolrParams solrQueryParams = new MultiMapSolrParams(requestParamsSolr);
+
+		QueryResponse solrResponse = solrClient //
+				.query(collection, solrQueryParams, METHOD.POST); // throws RemoteSolrException
+
+		System.err.println(solrResponse.jsonStr());
+
+	}
+
+}
---
+++ nlp4j/nlp4j-solr/src/test/java/hello/HelloSolr2.java
@@ -0,0 +1,55 @@
+package hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrRequest.METHOD;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.params.MultiMapSolrParams;
+
+/**
+ * Example of facet search
+ *
+ */
+public class HelloSolr2 {
+
+	public static void main(String[] args) throws Exception {
+
+		String endPoint = "http://localhost:8983/solr/";
+		String collection = "sandbox";
+
+		HttpSolrClient solrClient = new HttpSolrClient.Builder(endPoint) //
+				.withConnectionTimeout(10 * 1000)//
+				.withSocketTimeout(60 * 1000)//
+				.build();
+
+		final Map<String, String[]> requestParamsSolr = new HashMap<>();
+
+		requestParamsSolr.put("q", new String[] { "*:*" });
+		requestParamsSolr.put("q.op", new String[] { "OR" });
+		requestParamsSolr.put("rows", new String[] { "0" });
+		requestParamsSolr.put("facet", new String[] { "true" });
+		requestParamsSolr.put("facet.field", new String[] { "your_field_ss" });
+		requestParamsSolr.put("facet.mincount", new String[] { "1" });
+		requestParamsSolr.put("facet.limit", new String[] { "10" });
+
+		MultiMapSolrParams solrQueryParams = new MultiMapSolrParams(requestParamsSolr);
+
+		System.err.println(solrQueryParams.toQueryString());
+
+		QueryResponse solrResponse = solrClient //
+				.query(collection, solrQueryParams, METHOD.POST); // throws RemoteSolrException
+
+		System.err.println(solrResponse.jsonStr());
+
+		for (FacetField fl : solrResponse.getFacetFields()) {
+			System.err.println(fl.getName());
+			for (org.apache.solr.client.solrj.response.FacetField.Count cnt : fl.getValues()) {
+				System.err.println(cnt.getName() + "," + cnt.getCount());
+			}
+		}
+	}
+
+}
