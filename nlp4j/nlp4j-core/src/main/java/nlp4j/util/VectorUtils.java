4b137cc4936a6936f920ee056924970e9b85e165
Hiroki Oya <12620956+oyahiroki@users.noreply.github.com>
2024-11-09 20:18:34 +0900

    work20241109b

--- nlp4j/nlp4j-core/src/main/java/nlp4j/util/VectorUtils.java
+++ nlp4j/nlp4j-core/src/main/java/nlp4j/util/VectorUtils.java
@@ -23,4 +23,28 @@ public class VectorUtils {
 		}
 	}
 
+	/**
+	 * コサイン類似度を計算するメソッド
+	 * 
+	 * @since 1.3.7.15
+	 */
+	public static double cosineSimilarity(double[] vectorA, double[] vectorB) {
+
+		if (vectorA.length != vectorB.length) {
+			throw new IllegalArgumentException("vector_length_different");
+		}
+
+		double dotProduct = 0.0;
+		double normA = 0.0;
+		double normB = 0.0;
+
+		for (int i = 0; i < vectorA.length; i++) {
+			dotProduct += vectorA[i] * vectorB[i];
+			normA += Math.pow(vectorA[i], 2);
+			normB += Math.pow(vectorB[i], 2);
+		}
+
+		return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+	}
+
 }
--- nlp4j/nlp4j-core/src/test/java/nlp4j/util/VectorUtilsTestCase.java
+++ nlp4j/nlp4j-core/src/test/java/nlp4j/util/VectorUtilsTestCase.java
@@ -6,6 +6,17 @@ import junit.framework.TestCase;
 
 public class VectorUtilsTestCase extends TestCase {
 
+	public void testCosineSimilarity() throws Exception {
+		double[] targetVector = { 1.0, 2.0, 3.0 };
+		double[][] otherVectors = { { 4.0, 5.0, 6.0 }, { 1.0, 0.0, 1.0 }, { 7.0, 8.0, 9.0 } };
+
+		System.out.println("ターゲットベクトルとのコサイン類似度:");
+		for (int i = 0; i < otherVectors.length; i++) {
+			double similarity = VectorUtils.cosineSimilarity(targetVector, otherVectors[i]);
+			System.out.printf("ベクトル %d: %.4f%n", i + 1, similarity);
+		}
+	}
+
 	public void testNormalize001() {
 		double[] array = { 1.0, 2.0, 3.0 }; // 例として3次元ベクトル
 		VectorUtils.normalize(array);
