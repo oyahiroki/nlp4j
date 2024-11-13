a68096392b4c51d37e036833ab3026a7e285378b
Hiroki Oya <12620956+oyahiroki@users.noreply.github.com>
2024-11-13 12:45:12 +0900

    work20241113b

--- nlp4j/nlp4j-llm/src/test/java/nlp4j/llm/embeddings/EmbeddingAnnotatorMain.java
+++ nlp4j/nlp4j-llm/src/test/java/nlp4j/llm/embeddings/EmbeddingAnnotatorMain.java
@@ -1,22 +1,29 @@
 package nlp4j.llm.embeddings;
 
 import nlp4j.Document;
-import nlp4j.DocumentAnnotator;
 import nlp4j.DocumentBuilder;
 
 public class EmbeddingAnnotatorMain {
 
 	static public void main(String[] args) throws Exception {
-	
-		Document doc = (new DocumentBuilder()).text("今日はいい天気です").build();
-	
-		DocumentAnnotator ann = new EmbeddingAnnotator();
+
+		int max = 1000;
+
+		EmbeddingAnnotator ann = new EmbeddingAnnotator();
 		ann.setProperty("target", "text");
-	
-		ann.annotate(doc);
-	
-		System.err.println(doc.getAttributeAsListNumbers("vector").size());
-	
+
+		long time1 = System.currentTimeMillis();
+
+		for (int n = 0; n < max; n++) {
+			Document doc = (new DocumentBuilder()).text("今日はいい天気です " + n).build();
+			ann.annotate(doc);
+//			System.err.println(doc.getAttributeAsListNumbers("vector").size());
+		}
+		long time2 = System.currentTimeMillis();
+
+		System.err.println("time: " + (time2 - time1) + ", docs: " + max + ",average: "
+				+ ((double) (time2 - time1) / (double) max));
+
 	}
 
 }
