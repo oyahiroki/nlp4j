d1c81277f09dabf5511346ed682717f2788ceba8
Hiroki Oya <12620956+oyahiroki@users.noreply.github.com>
2025-03-29 21:57:26 +0900

    Update PropertyUtils.java

--- nlp4j/nlp4j-core/src/main/java/nlp4j/util/PropertyUtils.java
+++ nlp4j/nlp4j-core/src/main/java/nlp4j/util/PropertyUtils.java
@@ -1,11 +1,28 @@
 package nlp4j.util;
 
+import java.lang.invoke.MethodHandles;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
+ * <pre>
+ * 環境変数やシステムプロパティからプロパティ値を取得するユーティリティクラス。
+ * Utility class for retrieving property values from environment variables or system properties.
+ * </pre>
+ * 
  * @since 1.3.7.15
  */
 public class PropertyUtils {
 
+	static private final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+
 	/**
+	 * <pre>
+	     * 指定されたキーに対応するプロパティ値を環境変数またはシステムプロパティから取得します。
+	     * Retrieves the property value for the specified key from the environment variables or system properties.
+	 * </pre>
+	 * 
 	 * @param key
 	 * @return
 	 * @since 1.3.7.15
@@ -22,11 +39,18 @@ public class PropertyUtils {
 		if (value != null) {
 			return value;
 		}
-
+		logger.warn("NOT_SET: " + key);
 		return null;
 	}
 
 	/**
+	 * <pre>
+	     * 指定されたキーに対応するプロパティ値を環境変数またはシステムプロパティから取得します。
+	     * Retrieves the property value for the specified key from the environment variables or system properties.
+	     * 値が設定されていない場合は例外をスローします。
+	     * Throws an exception if the value is not set.
+	 * </pre>
+	 * 
 	 * @param key
 	 * @return
 	 * @since 1.3.7.18
